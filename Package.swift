ad1356a050e96df0b75bfc70f469688ce7933fba
Jem Alvarez <jemalvarez22@gmail.com>
2022-04-11 20:26:34 -0400

    Add onboarder

--- Package.swift
+++ Package.swift
@@ -9,17 +9,9 @@ let package = Package(
         .library( name: "JsHelper", targets: ["JsHelper"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/JemAlvarez/onboarder", branch: "master")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "JsHelper",
-            dependencies: []),
-        .testTarget(
-            name: "JsHelperTests",
-            dependencies: ["JsHelper"]),
+        .target(name: "JsHelper", dependencies: [.product(name: "Onboarder", package: "onboarder")]),
     ]
 )
