94d2a945e4d1e747547a53fbd65fa56b1ff19ab0
Jem Alvarez <jemalvarez22@gmail.com>
2022-04-12 03:23:35 -0400

    String extensions

---
+++ Sources/JsHelper/Shared/Extensions/StringExtensions.swift
@@ -0,0 +1,23 @@
+//
+
+import Foundation
+
+public extension String {
+    //MARK: - Get Date from String
+    /// String to date given a date format
+    func getDate(for format: String) -> Date {
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+        formatter.locale = .current
+        formatter.setLocalizedDateFormatFromTemplate(format)
+        return formatter.date(from: self) ?? Date()
+    }
+    
+    //MARK: - Is emoji
+    /// Whether a string contains an emoji or not
+    @available (iOS 10.2, *)
+    func isEmoji() -> Bool {
+        guard let firstScalar = unicodeScalars.first else { return false }
+        return firstScalar.properties.isEmoji && firstScalar.value > 0x238C
+    }
+}
