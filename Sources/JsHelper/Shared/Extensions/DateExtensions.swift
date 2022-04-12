edd22ed663125025fed82d13aabd91bf5f37ce88
Jem Alvarez <jemalvarez22@gmail.com>
2022-04-12 02:46:12 -0400

    Date extensions

---
+++ Sources/JsHelper/Shared/Extensions/DateExtensions.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+public extension Date {
+    //MARK: - Formats
+    /// Date String Formats
+    ///
+    /// * commaWithWeekday - Tuesday, Apr 12, 2022
+    /// * slashes - 04/12/2022
+    /// * comma - Apr 12, 2022
+    enum Formats: String {
+        case commaWithWeekday = "EEEE, MMM d, yyyy" // Tuesday, Apr 12, 2022
+        case slashes = "MM/dd/yyyy" // 04/12/2022
+        case comma = "MMM d, yyyy" // Apr 12, 2022
+    }
+    
+    //MARK: - Get String from Date
+    /// Get date string with date format
+    func getString(for format: String) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+        formatter.locale = .current
+        formatter.setLocalizedDateFormatFromTemplate(format)
+        return formatter.string(from: self)
+    }
+}
